67c9fd1af303613ab8d195bf4de110e54bea191f
ukgacts <167248362+toymachineuk@users.noreply.github.com>
2026-09-15 18:58:03 +0400

    up

---
+++ assets/js/movingletters.js
@@ -0,0 +1,50 @@
+var ml = {
+  timelines: {}
+};
+
+ml.timelines["ml8"] = anime.timeline({loop: true})
+  .add({
+    targets: '.tv-moving-letter .ml8 .circle-white',
+    scale: [0, 3],
+    opacity: [1, 0],
+    easing: "easeInOutExpo",
+    rotateZ: 360,
+    duration: 1100
+  }).add({
+    targets: '.tv-moving-letter .ml8 .circle-container',
+    scale: [0, 1],
+    duration: 1100,
+    easing: "easeInOutExpo",
+    offset: '-=1000'
+  }).add({
+    targets: '.tv-moving-letter .ml8 .circle-dark',
+    scale: [0, 1],
+    duration: 1100,
+    easing: "easeOutExpo",
+    offset: '-=600'
+  }).add({
+    targets: '.tv-moving-letter .letters-left',
+    scale: [0, 1],
+    duration: 1200,
+    offset: '-=550'
+  }).add({
+    targets: '.tv-moving-letter .bang',
+    scale: [0, 1],
+    rotateZ: [45, 15],
+    duration: 1200,
+    offset: '-=1000'
+  }).add({
+    targets: '.tv-moving-letter .ml8',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1400
+  });
+
+anime({
+  targets: '.tv-moving-letter .ml8 .circle-dark-dashed',
+  rotateZ: 360,
+  duration: 8000,
+  easing: "linear",
+  loop: true
+});
