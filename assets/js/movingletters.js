24f1d540447f15c6749d7fee10c2b6cd555fb8b7
ukgacts <167248362+toymachineuk@users.noreply.github.com>
2026-09-23 11:55:51 +0400

    up

--- assets/js/movingletters.js
+++ assets/js/movingletters.js
@@ -2,49 +2,95 @@ var ml = {
   timelines: {}
 };
 
-ml.timelines["ml8"] = anime.timeline({loop: true})
-  .add({
-    targets: '.tv-moving-letter .ml8 .circle-white',
-    scale: [0, 3],
-    opacity: [1, 0],
-    easing: "easeInOutExpo",
-    rotateZ: 360,
-    duration: 1100
-  }).add({
-    targets: '.tv-moving-letter .ml8 .circle-container',
-    scale: [0, 1],
-    duration: 1100,
-    easing: "easeInOutExpo",
-    offset: '-=1000'
-  }).add({
-    targets: '.tv-moving-letter .ml8 .circle-dark',
-    scale: [0, 1],
-    duration: 1100,
-    easing: "easeOutExpo",
-    offset: '-=600'
-  }).add({
-    targets: '.tv-moving-letter .letters-left',
-    scale: [0, 1],
-    duration: 1200,
-    offset: '-=550'
-  }).add({
-    targets: '.tv-moving-letter .bang',
-    scale: [0, 1],
-    rotateZ: [45, 15],
-    duration: 1200,
-    offset: '-=1000'
-  }).add({
-    targets: '.tv-moving-letter .ml8',
-    opacity: 0,
-    duration: 1000,
-    easing: "easeOutExpo",
-    delay: 1400
+ml.timelines["ml8"] = anime.timeline({
+  loop: false,
+  autoplay: false
+})
+
+.add({
+
+  targets: '.tv-moving-letter .ml8 .circle-white',
+
+  scale: [0, 3],
+  opacity: [1, 0],
+
+  easing: "easeInOutExpo",
+  rotateZ: 360,
+  duration: 1100
+
+}).add({
+
+  targets: '.tv-moving-letter .ml8 .circle-container',
+
+  scale: [0, 1],
+  duration: 1100,
+  easing: "easeInOutExpo",
+  offset: '-=1000'
+
+}).add({
+
+  targets: '.tv-moving-letter .ml8 .circle-dark',
+
+  scale: [0, 1],
+  duration: 1100,
+  easing: "easeOutExpo",
+  offset: '-=600'
+
+}).add({
+
+  targets: '.tv-moving-letter .letters-left',
+
+  scale: [0, 1],
+  duration: 1200,
+  offset: '-=550'
+
+}).add({
+  targets: '.tv-moving-letter .bang',
+  scale: [0, 1],
+  opacity: [0, 1],
+  rotateZ: 15,
+  duration: 1200,
+  offset: '-=1000'
+});
+
+/* =====================================
+   PLAY ONCE ON SCROLL
+===================================== */
+
+var movingLetterSection = document.querySelector('.tv-moving-letter');
+
+if (movingLetterSection) {
+
+  var movingLetterObserver = new IntersectionObserver(function(entries) {
+
+    if (entries[0].isIntersecting) {
+
+      ml.timelines["ml8"].play();
+
+      /* Never play again */
+      movingLetterObserver.unobserve(movingLetterSection);
+    }
+
+  }, {
+    threshold: 0.35
   });
 
+  movingLetterObserver.observe(movingLetterSection);
+}
+
+
+/* =====================================
+   GREEN DASHED CIRCLE
+   KEEPS ROTATING
+===================================== */
+
 anime({
+
   targets: '.tv-moving-letter .ml8 .circle-dark-dashed',
+
   rotateZ: 360,
   duration: 8000,
   easing: "linear",
   loop: true
+
 });
